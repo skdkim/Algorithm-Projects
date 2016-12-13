d2403fd2051af0a9dcc60ebb6f4dff0a077d57c8
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-12-12 21:19:07 -0800

    complete ring buffer

--- p1_dynamic_array/lib/ring_buffer.rb
+++ p1_dynamic_array/lib/ring_buffer.rb
@@ -4,30 +4,79 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    self.store = StaticArray.new(8)
+    self.capacity = 8
+    self.length = 0
+    self.start_idx = 0;
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    store[(start_idx + index) % capacity]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    store[(start_idx + index) % capacity] = val
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if length == 0
+
+    val = self[length - 1]
+    self[length - 1] = nil
+
+    self.length -= 1
+    val
   end
 
   # O(1) ammortized
   def push(val)
+    if length == capacity
+      resize!
+    end
+
+    self.length += 1
+    self[length - 1] = val
+    nil
   end
 
   # O(1)
   def shift
+    raise "index out of bounds" if length == 0
+
+    # (0...self.length).each do |num|
+    #   self.store[num] = self.store[num + 1]
+    # end
+
+    val = self[0]
+    self[0] = nil
+    self.start_idx = (start_idx + 1) % capacity
+
+    self.length -= 1
+    val
   end
 
   # O(1) ammortized
   def unshift(val)
+    if length == capacity
+      resize!
+    end
+      # self.length += 1
+      # count = self.length
+      # while count > 0
+      #   self.store[count] = self.store[count - 1]
+      #   count -= 1
+      # end
+      # self.store[0] = val
+
+      self.start_idx = (start_idx - 1) % capacity
+      self.length += 1
+      self[0] = val
+
   end
 
   protected
@@ -35,8 +84,17 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" if index < 0 || index >= length
   end
 
   def resize!
+    # self.capacity *= 2
+    new_capacity = capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    length.times { |i| new_store[i] = self[i] }
+
+    self.capacity = new_capacity
+    self.store = new_store
+    self.start_idx = 0
   end
 end
