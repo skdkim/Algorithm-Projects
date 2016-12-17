3de07ea19503f74489fc62559f235a0f1ad7eae0
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-12-17 14:52:49 -0800

    complete linked list

--- p2_hashmap/lib/p04_linked_list.rb
+++ p2_hashmap/lib/p04_linked_list.rb
@@ -14,7 +14,13 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,27 +29,57 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def insert(key, val)
+    each { |link| return link.val = val if link.key == key }
+
+    new_link = Link.new(key, val)
+
+    @tail.prev.next = new_link
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev = new_link
+
+    new_link
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+        link.next = nil
+        link.prev = nil
+        return link.val
+      end
+    end
   end
 
   def each
+    current_link = @head.next
+    until current_link == @tail
+      yield current_link
+      current_link = current_link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
