2e87f490ab755d1bc8b0de2e609dbe7d78c90463
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-12-17 14:09:07 -0800

    complete hash set

--- p2_hashmap/lib/p03_hash_set.rb
+++ p2_hashmap/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key.hash] << key
+    @count += 1
+    resize! if num_buckets < @count
+
+    key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return nil unless include?(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,10 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |key| insert(key) }
   end
 end
