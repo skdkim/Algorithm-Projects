72d31d05cfc4e0761e448d24acbb569c9f3c1845
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-12-17 19:11:18 -0800

    complete lru cache

--- p2_hashmap/lib/p06_lru_cache.rb
+++ p2_hashmap/lib/p06_lru_cache.rb
@@ -15,6 +15,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      link = @map[key]
+      update_link!(link)
+      link.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -25,12 +32,29 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    new_link = @store.insert(key, val)
+    @map[key] = new_link
+
+    eject! if count > @max
+    val
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    link.prev.next = link.next
+    link.next.prev = link.prev
+
+    link.prev = @store.last
+    link.next = @store.last.next
+    @store.last.next = link
   end
 
   def eject!
+    rm_link = @store.first
+    rm_link.prev.next = rm_link.next
+    rm_link.next.prev = rm_link.prev
+    @map.delete(rm_link.key)
+    nil
   end
 end
